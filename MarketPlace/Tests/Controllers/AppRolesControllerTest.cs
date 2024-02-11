b09db1dbbd26cb45d18726e31c08e2fdbb1c95de
danil.bespalov.00 <danil.bespalov.00@gmail.com>
2024-02-11 13:41:55 +0600

    Adding some tests for controller

---
+++ MarketPlace/Tests/Controllers/AppRolesControllerTest.cs
@@ -0,0 +1,61 @@
+﻿using MarketPlace.Controllers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+
+namespace MarketPlace.Tests.Controllers
+{
+    public class AppRolesControllerTests
+    {
+        [Test]
+        public async Task Index_ReturnsViewWithRoles()
+        {
+            // Arrange
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole("Admin"),
+                new IdentityRole("User")
+            };
+
+            var mockStore = new Mock<IQueryableRoleStore<IdentityRole>>();
+            var mockRoleManager = new Mock<RoleManager<IdentityRole>>(mockStore.Object, null, null, null, null);
+
+            mockRoleManager.Setup(m => m.Roles).Returns(roles.AsQueryable());
+
+            var controller = new AppRolesController(mockRoleManager.Object);
+
+            // Act
+            var result = controller.Index() as ViewResult;
+            var model = result?.Model as IEnumerable<IdentityRole>;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(model);
+            Assert.AreEqual(2, model.Count());
+        }
+
+
+        [Test]
+        public async Task Create_Post_ReturnsRedirectToIndex()
+        {
+            // Arrange
+            var role = new IdentityRole("TestRole");
+
+            var mockStore = new Mock<IQueryableRoleStore<IdentityRole>>();
+            var mockRoleManager = new Mock<RoleManager<IdentityRole>>(mockStore.Object, null, null, null, null);
+
+            mockRoleManager.Setup(m => m.RoleExistsAsync(role.Name)).ReturnsAsync(false);
+            mockRoleManager.Setup(m => m.CreateAsync(It.IsAny<IdentityRole>())).ReturnsAsync(IdentityResult.Success);
+
+            var controller = new AppRolesController(mockRoleManager.Object);
+
+            // Act
+            var result = await controller.Create(role) as RedirectToActionResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ActionName);
+        }
+    }
+}
---
+++ MarketPlace/Tests/Controllers/CategoryControllerTest.cs
@@ -0,0 +1,69 @@
+﻿using MarketPlace.Controllers;
+using MarketPlace.Data;
+using MarketPlace.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace MarketPlace.Tests.Controllers
+{
+    [TestFixture]
+    public class CategoryControllerTests
+    {
+        [Test]
+        public async Task Index_ReturnsViewWithCategories()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Categories.Add(new Category { CategoryId = 101, Name = "Category 1", CategoryThumbnail = "thumbnail1.jpg" });
+                context.Categories.Add(new Category { CategoryId = 102, Name = "Category 2", CategoryThumbnail = "thumbnail2.jpg" });
+                context.SaveChanges();
+
+                var controller = new CategoryController(context);
+
+                // Act
+                var result = await controller.Index() as ViewResult;
+                var model = result.Model as List<Category>;
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.IsNotNull(model);
+                Assert.AreEqual(3, model.Count);
+            }
+        }
+
+        [Test]
+        public async Task Details_WithValidId_ReturnsViewWithCategory()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Categories.Add(new Category { CategoryId = 1, Name = "Category 1", CategoryThumbnail = "thumbnail.jpg" });
+                context.SaveChanges();
+
+                var controller = new CategoryController(context);
+
+                // Act
+                var result = await controller.Details(1) as ViewResult;
+                var model = result.Model as Category;
+
+                // Assert
+                Assert.IsNotNull(result);
+                Assert.IsNotNull(model);
+                Assert.AreEqual("Category 1", model.Name);
+            }
+        }
+
+
+        // You can add more tests for other actions similarly
+    }
+}
